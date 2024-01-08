87ea96b63d4d46dc2302d677b8ed9d0e8828c00a
João Lee <joaumlimaum@gmail.com>
2024-01-08 17:19:00 -0300

    Added events to form inputs and buttons

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: 1,
+    conteudo: "Estudar Angular",
+    autoria: "Um dev" 
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("criado...")
+  }
+
+
+  cancelar() {
+    alert("cancelado...")
   }
 
 }
